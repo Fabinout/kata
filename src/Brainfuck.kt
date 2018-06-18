843b68cbcebe069fa459121d0880a0edf659f2a4
Fabien Lamarque <Fabien.Lamarque+github@gmail.com>
2018-06-18 13:30:46 +0200

    début du kata

---
+++ src/Brainfuck.kt
@@ -0,0 +1,5 @@
+fun eval(expr: String) =
+        print(expr)
+
+
+
---
+++ src/BrainfuckKtTest.kt
@@ -0,0 +1,11 @@
+import org.junit.Test
+import org.junit.Assert
+
+class TaskKtTest{
+
+    @Test fun testNum() {
+        eval("test")
+        assert(true)
+    }
+
+}
